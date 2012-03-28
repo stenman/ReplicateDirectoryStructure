ad97df04359e64e200eeb13c42dbb44d448d1a13
Roger <roger.stenman@gmail.com>
2012-03-28 08:25:21 +0200

    Added controls for checking directory existance

--- ReplicateFolderStructure.py
+++ ReplicateFolderStructure.py
@@ -1,4 +1,4 @@
-#coding=UTF-8
+﻿#coding=UTF-8
 import os
 import sys
 
@@ -7,6 +7,7 @@ print (os.getcwd())
 #TODO: L�gg dessa i en config?
 replicateFolderStructureQuestion = "Are you sure you want to replicate the cwd structure recursively?"
 defaultAnswerToReplicateFolderStructureQuery = "no"
+folderToBeReplicated = "/test/FolderToBeReplicated"
 defaultReplicationDestinationDirectory = "/ReplicatedFolderStructure"
 defaultWindowsRootFolder = "/"
 
@@ -24,11 +25,11 @@ def QueryYesNo(question, default="no"):
 	ClearScreen()
 	valid = {"yes": True, "y": True, "Y":True, "YES":True, "Yes":True, "no":False, "n":False, "N":False, "NO":False, "No":False}
 	if default == None:
-		prompt = " [y/n] "
+		prompt = " [y/n]: "
 	elif default == "yes":
-		prompt = " [Y/n] "
+		prompt = " [Y/n]: "
 	elif default == "no":
-		prompt = " [y/N] "
+		prompt = " [y/N]: "
 	else:
 		raise ValueError("Invalid default answer: '%S'" % default)
 
@@ -47,26 +48,32 @@ def QueryYesNo(question, default="no"):
 def main():
 	try:
 		if QueryYesNo(replicateFolderStructureQuestion, defaultAnswerToReplicateFolderStructureQuery):
-			#TODO: Kontrollera att den nya root-katalogen inte finns, 
-			#TODO: om den inte redan finns - forts�tt till sj�lva replikeringen (skapa inte mappen f�rr�n replikeringen gick bra!)
-			#TODO: om den redan finns - avsluta programmet
-			print("1.4")
-			os.mkdir(defaultReplicationDestinationDirectory)
-			os.chdir("/test/FolderToBeReplicated") #TODO: Byt detta statement till os.chdir(defaultReplicationFolder)
-			print("Replicating folder structure in", os.getcwd(), " recursively.\n")
-			print("You will find the root of the replicated folder structure here:\n", defaultReplicationDestinationDirectory)
+			if os.path.isdir(defaultReplicationDestinationDirectory):
+				raise OSError("The destination directory you want to replicate to already exist!")
+			if not os.path.isdir(folderToBeReplicated):
+				raise OSError("The folder you want to replicate does not exist!")
 			#TODO: starta replikeringen (skapa en lista (eller n�t) �ver mappstrukturen genom att f�rst TESTA att det ens �r m�jligt med os.access)
 			#TODO: kolla upp os.walk()!
+			
+			os.chdir(folderToBeReplicated)
+			
+			os.mkdir(defaultReplicationDestinationDirectory)
+			print("\nReplicating folder structure in", os.getcwd(), "recursively.")
+			print("\nYou will find the root of the replicated folder structure here:\n", defaultReplicationDestinationDirectory, "\n")
 			#TODO: skapa replikan i defaultReplicationDestinationDirectory (anv�nd listan som skapades)
 			
 		else:
-			print("Terminating program.")
+			print("\nTerminating program.")
 			sys.exit(0)
 
-	except Exception as error:
-		print("1An unexpected exception was encountered: %s", str(error))
+	except OSError as error:
+		print("\nOSError: %s" % str(error))
 		sys.exit(1)
-		
+
+	except Exception as error:
+		print("\nAn unexpected exception was encountered: %s" % str(error))
+		sys.exit(1)
+
 	finally:
 		#Test:
 		if os.path.isdir(defaultReplicationDestinationDirectory):
